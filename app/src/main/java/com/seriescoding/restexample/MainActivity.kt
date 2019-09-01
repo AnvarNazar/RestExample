9a59b16acf9869391baa7697fdf3b789fcbec2fe
Anvar Nazar <anvarnasar@ymail.com>
2019-09-01 22:39:30 +0530

    test user service

--- app/src/main/java/com/seriescoding/restexample/MainActivity.kt
+++ app/src/main/java/com/seriescoding/restexample/MainActivity.kt
@@ -2,11 +2,32 @@ package com.seriescoding.restexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.seriescoding.restexample.model.User
+import com.seriescoding.restexample.service.UserService
+import com.seriescoding.restexample.util.ServiceProvider
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val userService = ServiceProvider.getService(UserService::class.java)
+
+        userService.users().enqueue(object : Callback<List<User>> {
+
+            override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+                Log.d(packageName, "Users list fetched from the service")
+            }
+
+            override fun onFailure(call: Call<List<User>>, t: Throwable) {
+                Log.e(packageName, "Failed to fetch users list from service")
+            }
+        })
+
     }
 }
